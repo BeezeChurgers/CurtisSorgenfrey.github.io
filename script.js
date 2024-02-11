a4aa786751d0bf42b4a9f57d98a75fd90b26fee6
BeezeChurgers <curtissorgenfrey@gmail.com>
2024-02-11 15:00:12 -0800

    Logo design process

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Mobile responsive size
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
 function colorChanger() {
 	let x = document.getElementById("logo");
 	x.style.background = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
