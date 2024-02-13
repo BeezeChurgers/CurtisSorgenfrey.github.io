5af50865c433dea8a3f2e6ae37ed0baf944f6582
BeezeChurgers <curtissorgenfrey@gmail.com>
2024-02-12 16:19:09 -0800

    Add Logo animation

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Mobile responsive size
-var siteWidth = 1280;
-var scale = screen.width /siteWidth;
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
+// Changes logo color
 function colorChanger() {
 	let x = document.getElementById("logo");
 	x.style.background = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 }
+
+// Logo animation on click
