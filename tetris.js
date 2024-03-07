558e3efd42ac10b2e7731ae3ab6f78f8bc6c6834
BeezeChurgers <curtissorgenfrey@gmail.com>
2024-03-06 17:55:47 -0800

    Tetris!!!

--- tetris.js
+++ tetris.js
@@ -1,10 +1,28 @@
-// wsdocument.addEventListener('DOMContentLoaded', () => {
+const startBtn = document.querySelector("button")
+const grid = document.querySelector('.grid');
+const displaySquares = document.querySelectorAll(".nextGrid div");
+let squares = Array.from(grid.querySelectorAll('div'));
+const width = 10;
+const height = 20;
+let currentPosition = 4;
+let timerId;
 
-const grid = document.querySelector('.grid')
-let squares = Array.from(grid.querySelectorAll('div'))
-const width = 10
-const height = 20
-let currentPosition = 4
+// Control blocks
+let move = (event) => {
+	if (event.key === "d") {
+		moveRight();
+	}	else if (event.key === "a") {
+		moveLeft();
+	} else if (event.key === "s") {
+		moveDown();
+	} else if (event.key === "e") {
+		rotateRight();
+	} else if (event.key === "q") {
+		rotateLeft();
+	}
+}
+
+window.addEventListener("keypress", move);
 
 
 	// The Tetrominoes
@@ -15,11 +33,25 @@ let currentPosition = 4
 		[width, width * 2, width * 2 + 1, width * 2 + 2]
 	]
 	
+	const mirlTetromino = [
+		[1, width + 1, width * 2 + 1, 0],
+		[width + 2, width * 2, width * 2 + 1, width * 2 + 2],
+		[1, width + 1, width * 2 + 1, width * 2 + 2],
+		[width, width + 1, width + 2, width * 2]
+	]
+	
 	const zTetromino = [
 		[0, width, width + 1, width * 2 + 1],
 		[width + 1, width + 2, width * 2, width * 2 + 1],
 		[0, width, width + 1, width * 2 + 1],
 		[width + 1, width + 2, width * 2, width * 2 + 1]
+	]
+	
+	const mirzTetromino = [
+		[2, width + 1, width + 2, width * 2 + 1],
+		[width, width + 1, width * 2 + 2, width * 2 + 1],
+		[2, width + 1, width + 2, width * 2 + 1],
+		[width, width + 1, width * 2 + 2, width * 2 + 1]
 	]
 	
 	const tTetromino = [
@@ -43,25 +75,25 @@ let currentPosition = 4
 		[width, width + 1, width + 2, width + 3]
 	]
 	
-	const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+	const theTetrominoes = [lTetromino, mirlTetromino, zTetromino, mirzTetromino, tTetromino, oTetromino, iTetromino]
 	
 	// Randomly select tetromino
-	let random = Math.floor(Math.random()*theTetrominoes.length)
-	let currentRotation = 0
-	let current = theTetrominoes[random][currentRotation]
+	let random = Math.floor(Math.random()*theTetrominoes.length);
+	let currentRotation = 0;
+	let current = theTetrominoes[random][currentRotation];
 	
 	// Draw the shape
 	function draw() {
 		current.forEach( index => (
 			squares[currentPosition + index].classList.add('block')
-		))
+		));
 	}
 	
 	// Undraw the shape
 	function undraw() {
 		current.forEach( index => (
 			squares[currentPosition + index].classList.remove('block')
-		))
+		));
 	}
 	
 	// Move down shape
@@ -69,53 +101,103 @@ let currentPosition = 4
 		undraw();
 		currentPosition += width;
 		draw();
+		freeze();
 	}
 
-    // Move right shape
+  // Move right shape
 	function moveRight() {
 		undraw();
-		currentPosition += 1;
+		const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
+		if (!isAtRightEdge) {
+			currentPosition += 1;
+		}
+		if (current.some(index => squares[currentPosition + index].classList.contains("block2"))) {
+			currentPosition -= 1;
+		}
 		draw();
 	}
 
-    // Move left shape
+  // Move left shape
 	function moveLeft() {
 		undraw();
-		currentPosition -= 1;
+		const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+		if (!isAtLeftEdge) {
+			currentPosition -= 1;
+		}
+		if (current.some(index => squares[currentPosition + index].classList.contains("block2"))) {
+			currentPosition += 1;
+		}
 		draw();
 	}
+	
+	// Rotate shape right
+	function rotateRight() {
+		undraw();
+		currentRotation = (currentRotation + 1) % 4;
+		current = theTetrominoes[random][currentRotation];
+		draw();
+	}
+	
+	// Rotate shape right
+	function rotateLeft() {
+		undraw();
+		currentRotation = (currentRotation + 3) % 4;
+		current = theTetrominoes[random][currentRotation];
+		draw();
+	}
+
+	// Show next Tetromino
+	const displayWidth = 4;
+	const displayIndex = 0;
+	let nextRandom = 0;
+
+	const smallTetrominoes = [
+		[1, width + 1, width * 2 + 1, 2],
+		[1, width + 1, width * 2 + 1, 0],
+		[0, width, width + 1, width * 2 + 1],
+		[2, width + 1, width + 2, width * 2 + 1],
+		[1, width, width + 1, width + 2],
+		[0, 1, width, width + 1],
+		[1, width + 1, width * 2 + 1, width * 3 + 1]
+	]
+
+	function displayShape() {
+		displaySquares.forEach(square => {
+			square.classList.remove("block");
+		});
+		smallTetrominoes[nextRandom].forEach(index => {
+			displaySquares[displayIndex + index].classList.add("block")
+		});
+	}
+
+	function freeze() {
+		if(current.some(index => squares[currentPosition + index + width].classList.contains("block3") || squares[currentPosition + index + width].classList.contains("block2"))) {
+			current.forEach(index => squares[index + currentPosition].classList.add("block2"));
+			random = nextRandom;
+			nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+			current = theTetrominoes[random][currentRotation];
+			currentPosition = 4;
+			draw();
+			displayShape();
+		}
+	}
+
+	startBtn.addEventListener("click", () => {
+		if(timerId) {
+			clearInterval(timerId);
+			timerId = null;
+		} else {
+			draw();
+			timerId = setInterval(moveDown, 1000);
+			nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+			displayShape();
+		}
+	});
+
+
 	
 	// Creating Swiping trigger
 	// Getting Horizontal Movement of Mouse
 	let getMouseX = (event) => event.movementX;
 	// Getting vertical Movement of Mouse
 	let getMouseY = (event) => event.movementY;
-	
-	// Initializing grid array
-	let gridArr = new Array(171);
-	for (let i = 0; i<gridArr.length; i++) {
-		gridArr[i] = 0;
-	}
-	
-	// Testing movement
-	let move = (event) => {
-		if (event.key === "d") {
-			if ((currentPosition % 10) != 9) {
-				moveRight();
-			}
-		}	else if (event.key === "a") {
-			if ((currentPosition % 10) != 0) {
-				moveLeft();
-			}
-		} else if (event.key === "s") {
-			if ((currentPosition + 10) < 171) {
-				moveDown();
-			}
-		}
-	}
-	
-	window.addEventListener("keypress", move);
-	
-/*}
-
-)*/
