a1a9411f0554e2935ba867d9166387fee94547f0
BeezeChurgers <curtissorgenfrey@gmail.com>
2024-05-20 11:35:31 -0700

    Applied game over to actual button

--- projects/snakegame.js
+++ projects/snakegame.js
@@ -209,9 +209,9 @@ const pause = () => {
       }
     });
     // Touch Controls
-    $(window).unbind('touchstart', handleTouchStart, false);
-    $(window).unbind('touchmove', handleTouchMove, false);
-    $(window).unbind('touchend', handleTouchEnd, false);
+    $('table').unbind('touchstart', handleTouchStart, false);
+    $('table').unbind('touchmove', handleTouchMove, false);
+    $('table').unbind('touchend', handleTouchEnd, false);
   } else {
     // Start timer
     ticker = setInterval(updateSnakeCell, speed);
@@ -223,9 +223,9 @@ const pause = () => {
       }
     });
     // Touch Controls
-    $(window).bind('touchstart', handleTouchStart, false);
-    $(window).bind('touchmove', handleTouchMove, false);
-    $(window).bind('touchend', handleTouchEnd, false);
+    $('table').bind('touchstart', handleTouchStart, false);
+    $('table').bind('touchmove', handleTouchMove, false);
+    $('table').bind('touchend', handleTouchEnd, false);
   }
 }
 
@@ -339,11 +339,13 @@ $(document).ready(function () {
   });
 
   // Touch Controls
-  $(window).bind('touchstart', handleTouchStart, false);
-  $(window).bind('touchmove', handleTouchMove, false);
-  $(window).bind('touchend', handleTouchEnd, false);
+  $('table').bind('touchstart', handleTouchStart, false);
+  $('table').bind('touchmove', handleTouchMove, false);
+  $('table').bind('touchend', handleTouchEnd, false);
 
   startGame();
   $('#pause').bind('click', pause);
-  $('table').click();
+
+  // Reloads game when clicked
+  $('div.gameOver').click();
 });
